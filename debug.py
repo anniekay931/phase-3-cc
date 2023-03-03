d06c828eed86429b7befc0ecea8e1d3b6917d6eb
Annika Kriskovich <annika.krisko2013@gmail.com>
2023-03-03 11:38:13 -0500

    12 tests passing

--- debug.py
+++ debug.py
@@ -4,11 +4,13 @@ import ipdb;
 
 if __name__ == '__main__':
 #  WRITE YOUR TEST CODE HERE ###
-
+    from Movie import Movie
     m = Movie("The Matrix")
     print(m.title())
 
-
+## from Movie import Movie
+## m = Movie("The Matrix: Reloaded"
+# print(m.title())
 
 
 # DO NOT REMOVE THIS
--- lib/Movie.py
+++ lib/Movie.py
@@ -1,9 +1,12 @@
 class Movie:
+
+    all = []
     
     def __init__(self, title, reviews=None, reviewers=None):
         self.title = title
         self.reviews = [] if reviews is None else reviews
         self.reviewers = [] if reviewers is None else reviewers
+        Movie.all.append(self)
 
     @property
     def title(self):
@@ -15,7 +18,7 @@ class Movie:
             self._title = new_title
         else:
             raise ValueError("Title must be a string")
-            
+
     def reviews(self):
         return self._reviews
     
@@ -28,10 +31,10 @@ class Movie:
         return sum(self.reviews) / len(self.reviews)
 
     @classmethod
-    def highest_rated(cls, movies):
+    def highest_rated(cls):
         highest_rated_movie = None
         highest_rating = 0
-        for movie in movies:
+        for movie in cls.all:
             rating = movie.average_rating()
             if rating and rating > highest_rating:
                 highest_rating = rating
