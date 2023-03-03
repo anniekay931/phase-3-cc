bae83909afd9e464e758e73ecae7cdada67ef754
Annika Kriskovich <annika.krisko2013@gmail.com>
2023-03-03 10:55:48 -0500

    8 tests passing

--- lib/Movie.py
+++ lib/Movie.py
@@ -1,21 +1,39 @@
 class Movie:
     
-    def __init__(self, title):
-        self._title = None
-        self.set_title(title)
+    def __init__(self, title, reviews=None, reviewers=None):
+        self.title = title
+        self.reviews = [] if reviews is None else reviews
+        self.reviewers = [] if reviewers is None else reviewers
 
+    @property
     def title(self):
         return self._title
     
-    def set_title(self, new_title):
+    @title.setter
+    def title(self, new_title):
         if isinstance(new_title, str) and len(new_title) > 0:
             self._title = new_title
         else:
             raise ValueError("Title must be a string")
 
+    def reviews(self):
+        return self._reviews
+    
+    def reviewers(self):
+        return [review.viewer() for review in self._reviews]
+    
     def average_rating(self):
-        pass
+        if not self.reviews:
+            return None
+        return sum(self.reviews) / len(self.reviews)
 
     @classmethod
-    def highest_rated(cls):
-        pass
+    def highest_rated(cls, movies):
+        highest_rated_movie = None
+        highest_rating = 0
+        for movie in movies:
+            rating = movie.average_rating()
+            if rating and rating > highest_rating:
+                highest_rating = rating
+                highest_rated_movie = movie
+        return highest_rated_movie
--- lib/Review.py
+++ lib/Review.py
@@ -1,38 +1,36 @@
+from viewer import Viewer
+from movie import Movie
+
 class Review:
-    
-    def __init__(self, viewer, movie, rating):
+
+    def __init__(self, rating, viewer, movie):
+        self._rating = rating
         self._viewer = None
         self._movie = None
-        self._rating = None
-        self.set_rating(rating)
+        self.set_viewer(viewer)
         self.set_movie(movie)
-        self.set_rating(rating)
 
-    def rating(self):
+    def get_rating(self):
         return self._rating
-    
-    def set_rating(self, new_rating):
-        if isinstance(new_rating, int) and 1 <= new_rating <= 5:
-            self._rating = new_rating
-        else:
-            raise ValueError("Rating must be an integer between 1 and 5")
 
-    def movie(self):
-        return self._movie
-    
-    def set_movie(self, new_movie):
-        if isinstance(new_movie, Movie):
-            self._movie = new_movie
-        else:
-            raise ValueError("Movie must be a Movie instance")
-    def rating(self):
-        return self._rating
-    
     def set_rating(self, new_rating):
-        if isinstance(new_rating, int) and 1 <= new_rating <= 5:
-            self._rating = new_rating
-        else:
-            raise ValueError("Rating must be an integer between 1 and 5")
-    
-    def viewer(self):
+        if new_rating < 1 or new_rating > 5:
+            raise ValueError("Rating must be between 1 and 5, inclusive")
+        self._rating = new_rating
+
+    def get_viewer(self):
         return self._viewer
+
+    def set_viewer(self, new_viewer):
+        if not isinstance(new_viewer, Viewer):
+            raise TypeError("Viewer must be an instance of Viewer class")
+        self._viewer = new_viewer
+
+    def get_movie(self):
+        return self._movie
+
+    def set_movie(self, new_movie):
+        if not isinstance(new_movie, Movie):
+            raise TypeError("Movie must be an instance of Movie class")
+        self._movie = new_movie
+
